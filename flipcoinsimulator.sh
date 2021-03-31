31f5a7b89c5f54f58df12edb7bdceb63d61a51ba
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-03-31 11:59:33 +0530

    Added: first UC added

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,1 +1,13 @@
-echo "welcome to flip coin simulation problem"
+
+#!/bin/bash -x
+declare -A coin
+	echo "${coin[@]}"
+
+toss=$((RANDOM%2))
+
+if [[ $toss -eq 0 ]]; then
+	echo "Heads"
+else
+	echo "Tails"
+fi
+
