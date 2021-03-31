e5641c889718d3e0490c1dea3aa8244e27de4e20
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-03-31 12:04:29 +0530

    Added: program to show H/T winner

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -11,3 +11,31 @@ else
 	echo "Tails"
 fi
 
+
+declare -A coin
+	echo "${coin[@]}"
+
+
+read -p "Enter 0 for Heads and 1 for Tails:" choice
+
+i=0
+while [[ $i -lt 50 ]]; do
+	toss=$((RANDOM%2))
+	if [[ $toss -eq 0 ]]; then
+		coin['Heads']=$((coin['Heads'] + 1))
+	else
+		coin['Tails']=$((coin['Tails'] + 1))
+	fi
+	((i++))
+done
+
+echo "Keys:   ${!coin[@]}"
+echo "Values: ${coin[@]}"
+
+if [[ $((coin['Heads'])) -eq $((coin['Tails'])) ]]; then
+	echo "It is a draw."
+elif [[ $((coin['Heads'])) -gt $((coin['Tails'])) && $choice -eq 0 ]]; then
+	echo "You Won."
+else
+	echo "You Lost."
+fi
